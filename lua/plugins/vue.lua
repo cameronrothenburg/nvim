fc7e217a7ec8e6e227f956d2cff4cac750a928ab
Cameron-Lee Rothenburg <cam.hind98@gmail.com>
2024-06-03 15:43:15 +0100

    Added vue

---
+++ lua/plugins/vue.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "posva/vim-vue",
+  },
+  {
+    "neoclide/coc.nvim",
+  },
+  {
+    "yaegassy/coc-volar-tools",
+    dependencies = {
+      "yaegassy/coc-volar",
+    },
+  },
+}
